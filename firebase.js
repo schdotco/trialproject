b8a9c031dd8afcda49b5a44b56bfb8aebce5e83e
Puskesmas Padasua <schdotco@gmail.com>
2026-01-12 19:36:04 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,27 @@
-<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
+// 🔐 Firebase Configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyBk57WWCAV9PbUQBb_bj7mPtl09oNz7WnA",
   authDomain: "schesakra.firebaseapp.com",
-  projectId: "schesakra"
+  databaseURL: "https://schesakra-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "schesakra",
+  storageBucket: "schesakra.appspot.com",
+  messagingSenderId: "672537227977",
+  appId: "1:672537227977:web:88b24f905c86eba7c05318"
 };
 
+// 🚀 Init Firebase
 export const app = initializeApp(firebaseConfig);
+
+// 📦 Firestore (UNTUK DATA AGREGAT DASHBOARD)
 export const db = getFirestore(app);
-</script>
+
+// 🔁 Realtime Database (OPSIONAL)
+export const rtdb = getDatabase(app);
+
+// 🔑 Authentication (UNTUK LOGIN ADMIN)
+export const auth = getAuth(app);
